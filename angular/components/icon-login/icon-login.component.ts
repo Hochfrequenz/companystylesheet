4248c22932592231dedea846f7854bd837b1723e
kevin <kevin.krechan@hochfrequenz.de>
2025-03-28 19:40:35 +0100

    add login icon

---
+++ angular/components/icon-login/icon-login.component.ts
@@ -0,0 +1,9 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-login',
+  imports: [],
+  templateUrl: '../../../icons/login.svg',
+  schemas: [NO_ERRORS_SCHEMA],
+})
+export class IconLoginComponent {}
