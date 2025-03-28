6095bee81e32d8e09bc2d1831c68309c1e4c69a3
kevin <kevin.krechan@hochfrequenz.de>
2025-03-28 19:48:25 +0100

    make component standalone

--- angular/components/icon-login/icon-login.component.ts
+++ angular/components/icon-login/icon-login.component.ts
@@ -1,9 +1,9 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-icon-login',
+  standalone: true,
   imports: [],
   templateUrl: '../../../icons/login.svg',
-  schemas: [NO_ERRORS_SCHEMA],
 })
 export class IconLoginComponent {}
